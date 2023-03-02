e9c903f9778581fb3340e33bf7d73081d5dd4810
Sandro Fernandes <sandrofernandesrosal@gail.com>
2023-03-01 23:40:19 -0300

    first commit

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+
+export const Button = ({title, loading = false, ...rest}) => {
+
+  return(
+    <Container type="button" disabled={loading} {...rest}>
+      
+      {loading ? 'Carregando...' : title}
+    </Container>
+  )
+}
---
+++ src/components/ButtonText/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export const ButtonText = ({ title, isActive = false, ...rest}) => {
+  return(
+    <Container type="button" {...rest} isActive={isActive}>
+      {title}
+    </Container>
+  )
+}
