f883be658544665ce04de59b396cbda811a99c66
Sandro Fernandes <sandrofernandesrosal@gail.com>
2023-03-16 01:18:11 -0300

    conectando ao backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'mynotes-mj7u5i4pp-sandrofernandesrosal.vercel.app'
+  baseURL: 'https://mynotes-6j4gde72j-sandrofernandesrosal.vercel.app'
 });
